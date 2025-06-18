48e6fa04e9f6b9de1576cab34198cfa19df8acbc
Achmad Rifa'i <officialrdevel@gmail.com>
2025-06-18 11:29:24 +0700

    go defers

--- api/index.go
+++ api/index.go
@@ -1,12 +1,22 @@
 package handler
 
 import (
+	"log"
 	"net/http"
 	"os"
 	"park/lib"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	defer lib.DefaultCatch(w)
 	os.Setenv("TZ", "Asia/Jakarta")
-	lib.SendJson(map[string]string{"msg": "Hellow world"}, w)
+	header := r.Header
+	if header.Get("ai-path") != "" {
+		myPath := header.Get("ai-path")
+		log.Printf("[%s] /api/%s", r.Method, myPath)
+		lib.SendJson(map[string]string{"msg": "Hello world"}, w)
+	} else {
+		log.Printf("[%s] /api/", r.Method)
+		lib.SendJson(map[string]string{"msg": "Hellow world"}, w)
+	}
 }
--- lib/send.go
+++ lib/send.go
@@ -1,9 +1,97 @@
 package lib
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"net/url"
+	"runtime/debug"
+	"strings"
 )
+
+func StmtClose(stmt *sql.Stmt, w http.ResponseWriter) {
+	DefaultCatch(w)
+	if stmt != nil {
+		if err := stmt.Close(); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func RowsClose(rows *sql.Rows, w http.ResponseWriter) {
+	DefaultCatch(w)
+	if rows != nil {
+		if err := rows.Close(); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func DbClose(db *sql.DB, w http.ResponseWriter) {
+	DefaultCatch(w)
+	if db != nil {
+		if err := db.Close(); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func TxClose(tx *sql.Tx, w http.ResponseWriter) {
+	if tx != nil {
+		if r := recover(); r != nil {
+			msg := fmt.Sprint("", r)
+			log.Println(msg)
+			log.Println(string(debug.Stack()))
+			if strings.HasPrefix(msg, "method") {
+				w.WriteHeader(405)
+			} else if strings.HasPrefix(msg, "Token") {
+				w.WriteHeader(403)
+			} else if strings.HasPrefix(msg, "bad:") {
+				w.WriteHeader(400)
+			} else {
+				w.WriteHeader(500)
+			}
+			if err := tx.Rollback(); err != nil {
+				panic(err)
+			}
+			SendJson(map[string]string{"msg": msg}, w)
+		} else {
+			if err := tx.Commit(); err != nil {
+				panic(err)
+			}
+		}
+	} else {
+		DefaultCatch(w)
+	}
+}
+
+func QueryToMap(query url.Values) map[string]string {
+	result := map[string]string{}
+	for k, v := range query {
+		result[k] = strings.Join(v, ", ")
+	}
+	return result
+}
+
+func DefaultCatch(w http.ResponseWriter) {
+	if r := recover(); r != nil {
+		msg := fmt.Sprint("", r)
+		log.Println(msg)
+		log.Println(string(debug.Stack()))
+		if strings.HasPrefix(msg, "method") {
+			w.WriteHeader(405)
+		} else if strings.HasPrefix(msg, "Token") {
+			w.WriteHeader(403)
+		} else if strings.HasPrefix(msg, "bad:") {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		SendJson(map[string]string{"msg": msg}, w)
+	}
+}
 
 func SendJson(resp any, w http.ResponseWriter) {
 	jsonData, err := json.Marshal(resp)
